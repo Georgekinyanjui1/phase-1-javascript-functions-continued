697227f706fdb8309263b4d9dd3966711390a6b9
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-08 18:09:41 +0300

    commit tree ab1755decf30e898a68ed598d6b92775a55e7f11

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(exist = "roller-skate") {
+    return `This Saturday, I want to ${exist}!`
+}
+
+function mondayWork(exist = 'go to the office') {
+    return `This Monday, I will ${exist}.`
+
+}
+
+function wrapAdjective(style) {
+    let result = (function(compliment) {
+        return "You are " + style + compliment + style + "!"
+    })
+    return result
+}
